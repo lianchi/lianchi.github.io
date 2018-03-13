d03a4621bf8c74b3963928f698159f1d17860804
zhoulianchi <peaner@qq.com>
2018-03-13 15:13:15 +0800

    fix

--- dist/static/js/39.7a43459a70fccfa154e4.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([39],{aeFh:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var r=n("exJd"),u=n("VU/8"),l=u(null,r.a,!1,null,null,null);t.default=l.exports},exJd:function(e,t,n){"use strict";var r=function(){var e=this,t=e.$createElement;return(e._self._c||t)("router-view")},u=[],l={render:r,staticRenderFns:u};t.a=l}});
